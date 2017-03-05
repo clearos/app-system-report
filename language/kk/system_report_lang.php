4f566bc6b1bc6baadd859788326b50cefb5992a7
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 02:18:58 +0000

    Add new files and directories and commit changes to local repository

--- language/kk/system_report_lang.php
+++ language/kk/system_report_lang.php
@@ -1,20 +1,20 @@
 <?php
 
-$lang['system_report_app_description'] = 'The System Report provides information on the operating system and underlying hardware.';
-$lang['system_report_app_name'] = 'System Report';
-$lang['system_report_available'] = 'Available';
-$lang['system_report_cpu_model'] = 'CPU Model';
-$lang['system_report_filesystem'] = 'Filesystem';
-$lang['system_report_filesystem_summary'] = 'Filesystem Summary';
-$lang['system_report_item'] = 'Item';
-$lang['system_report_kernel_version'] = 'Kernel Version';
-$lang['system_report_load'] = 'Load';
-$lang['system_report_memory_size'] = 'Memory Size';
+$lang['system_report_app_description'] = 'Жүйе туралы есеп операциялық жүйеге және базалық аппараттық туралы ақпарат береді.';
+$lang['system_report_app_name'] = 'Жүйе туралы есеп';
+$lang['system_report_available'] = 'Қол жетімді';
+$lang['system_report_cpu_model'] = 'CPU моделі';
+$lang['system_report_filesystem'] = 'Файлдық жүйесі';
+$lang['system_report_filesystem_summary'] = 'Файлдық жүйесі Резюме';
+$lang['system_report_item'] = 'тармақ';
+$lang['system_report_kernel_version'] = 'ядро нұсқасы';
+$lang['system_report_load'] = 'жүк';
+$lang['system_report_memory_size'] = 'жад мөлшері';
 $lang['system_report_mounted'] = 'Mounted';
-$lang['system_report_size'] = 'Size';
-$lang['system_report_system_details'] = 'System Details';
-$lang['system_report_system_time'] = 'System Time';
+$lang['system_report_size'] = 'Өлшем';
+$lang['system_report_system_details'] = 'Жүйе Толығырақ';
+$lang['system_report_system_time'] = 'Жүйе уақыты';
 $lang['system_report_uptime'] = 'Uptime';
-$lang['system_report_use'] = 'Use %';
-$lang['system_report_used'] = 'Used';
-$lang['system_report_value'] = 'Value';
+$lang['system_report_use'] = '% пайдалану';
+$lang['system_report_used'] = 'Қолданылған';
+$lang['system_report_value'] = 'құн';
