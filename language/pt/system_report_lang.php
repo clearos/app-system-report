66cff1e58d83109c8caf9202fe7c4e3a32f22438
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 02:22:55 +0000

    Add new files and directories and commit changes to local repository

--- language/pt/system_report_lang.php
+++ language/pt/system_report_lang.php
@@ -1,20 +1,20 @@
 <?php
 
-$lang['system_report_app_description'] = 'The System Report provides information on the operating system and underlying hardware.';
-$lang['system_report_app_name'] = 'System Report';
-$lang['system_report_available'] = 'Available';
-$lang['system_report_cpu_model'] = 'CPU Model';
-$lang['system_report_filesystem'] = 'Filesystem';
-$lang['system_report_filesystem_summary'] = 'Filesystem Summary';
+$lang['system_report_app_description'] = 'O Relatório de sistema fornece informações sobre o sistema operacional e hardware subjacente.';
+$lang['system_report_app_name'] = 'Relatório do sistema';
+$lang['system_report_available'] = 'acessível';
+$lang['system_report_cpu_model'] = 'Modelo CPU';
+$lang['system_report_filesystem'] = 'Sistema de arquivo';
+$lang['system_report_filesystem_summary'] = 'Resumo do sistema de arquivos';
 $lang['system_report_item'] = 'Item';
-$lang['system_report_kernel_version'] = 'Kernel Version';
-$lang['system_report_load'] = 'Load';
-$lang['system_report_memory_size'] = 'Memory Size';
-$lang['system_report_mounted'] = 'Mounted';
-$lang['system_report_size'] = 'Size';
-$lang['system_report_system_details'] = 'System Details';
-$lang['system_report_system_time'] = 'System Time';
-$lang['system_report_uptime'] = 'Uptime';
-$lang['system_report_use'] = 'Use %';
-$lang['system_report_used'] = 'Used';
-$lang['system_report_value'] = 'Value';
+$lang['system_report_kernel_version'] = 'Kernel Versão';
+$lang['system_report_load'] = 'Carga';
+$lang['system_report_memory_size'] = 'Tamanho da memória';
+$lang['system_report_mounted'] = 'Montado';
+$lang['system_report_size'] = 'Tamanho';
+$lang['system_report_system_details'] = 'Detalhes do sistema';
+$lang['system_report_system_time'] = 'Time System';
+$lang['system_report_uptime'] = 'Tempo de atividade';
+$lang['system_report_use'] = 'Usar %';
+$lang['system_report_used'] = 'Usava';
+$lang['system_report_value'] = 'Valor';
