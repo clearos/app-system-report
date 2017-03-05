90937555c1f223b63044e1f21ac44eab4ab1f893
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 02:19:41 +0000

    Add new files and directories and commit changes to local repository

--- language/la/system_report_lang.php
+++ language/la/system_report_lang.php
@@ -1,20 +1,20 @@
 <?php
 
-$lang['system_report_app_description'] = 'The System Report provides information on the operating system and underlying hardware.';
-$lang['system_report_app_name'] = 'System Report';
+$lang['system_report_app_description'] = 'Ratio est Relatio operating ratio praebet notitia subiecti et odio.';
+$lang['system_report_app_name'] = 'ratio Report';
 $lang['system_report_available'] = 'Available';
-$lang['system_report_cpu_model'] = 'CPU Model';
-$lang['system_report_filesystem'] = 'Filesystem';
-$lang['system_report_filesystem_summary'] = 'Filesystem Summary';
+$lang['system_report_cpu_model'] = 'Model CPU';
+$lang['system_report_filesystem'] = 'filesystem';
+$lang['system_report_filesystem_summary'] = 'Summary filesystem';
 $lang['system_report_item'] = 'Item';
-$lang['system_report_kernel_version'] = 'Kernel Version';
-$lang['system_report_load'] = 'Load';
-$lang['system_report_memory_size'] = 'Memory Size';
-$lang['system_report_mounted'] = 'Mounted';
+$lang['system_report_kernel_version'] = 'Vulgate kernel';
+$lang['system_report_load'] = 'load';
+$lang['system_report_memory_size'] = 'Memoria Size';
+$lang['system_report_mounted'] = 'equites';
 $lang['system_report_size'] = 'Size';
-$lang['system_report_system_details'] = 'System Details';
-$lang['system_report_system_time'] = 'System Time';
-$lang['system_report_uptime'] = 'Uptime';
-$lang['system_report_use'] = 'Use %';
+$lang['system_report_system_details'] = 'Details System';
+$lang['system_report_system_time'] = 'System';
+$lang['system_report_uptime'] = 'uptime';
+$lang['system_report_use'] = 'Uti%';
 $lang['system_report_used'] = 'Used';
 $lang['system_report_value'] = 'Value';
