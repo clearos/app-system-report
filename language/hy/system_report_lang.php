5659887965bec4c1b253be3d6daf45a3a354520d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 02:13:06 +0000

    Add new files and directories and commit changes to local repository

--- language/hy/system_report_lang.php
+++ language/hy/system_report_lang.php
@@ -1,20 +1,20 @@
 <?php
 
-$lang['system_report_app_description'] = 'The System Report provides information on the operating system and underlying hardware.';
-$lang['system_report_app_name'] = 'System Report';
-$lang['system_report_available'] = 'Available';
-$lang['system_report_cpu_model'] = 'CPU Model';
-$lang['system_report_filesystem'] = 'Filesystem';
-$lang['system_report_filesystem_summary'] = 'Filesystem Summary';
-$lang['system_report_item'] = 'Item';
-$lang['system_report_kernel_version'] = 'Kernel Version';
-$lang['system_report_load'] = 'Load';
-$lang['system_report_memory_size'] = 'Memory Size';
-$lang['system_report_mounted'] = 'Mounted';
-$lang['system_report_size'] = 'Size';
-$lang['system_report_system_details'] = 'System Details';
+$lang['system_report_app_description'] = 'Համակարգը զեկույցը տրամադրում է տեղեկատվություն օպերացիոն համակարգի եւ հիմքում ընկած hardware.';
+$lang['system_report_app_name'] = 'System հաշվետվություն';
+$lang['system_report_available'] = 'մատչելի';
+$lang['system_report_cpu_model'] = 'CPU մոդել';
+$lang['system_report_filesystem'] = 'filesystem';
+$lang['system_report_filesystem_summary'] = 'filesystem Ամփոփում';
+$lang['system_report_item'] = 'կետ';
+$lang['system_report_kernel_version'] = 'Kernel Տարբերակ';
+$lang['system_report_load'] = 'բեռնել';
+$lang['system_report_memory_size'] = 'հիշողության Չափ';
+$lang['system_report_mounted'] = 'հեծյալ';
+$lang['system_report_size'] = 'չափ';
+$lang['system_report_system_details'] = 'System Մանրամասներ';
 $lang['system_report_system_time'] = 'System Time';
-$lang['system_report_uptime'] = 'Uptime';
-$lang['system_report_use'] = 'Use %';
-$lang['system_report_used'] = 'Used';
-$lang['system_report_value'] = 'Value';
+$lang['system_report_uptime'] = 'uptime';
+$lang['system_report_use'] = 'օգտագործել%';
+$lang['system_report_used'] = 'Օգտագործված';
+$lang['system_report_value'] = 'արժեք';
