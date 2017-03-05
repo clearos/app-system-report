2c50809ba57ed0c006b931158957ed7f6a1c732d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 02:24:47 +0000

    Add new files and directories and commit changes to local repository

--- language/su/system_report_lang.php
+++ language/su/system_report_lang.php
@@ -1,20 +1,20 @@
 <?php
 
-$lang['system_report_app_description'] = 'The System Report provides information on the operating system and underlying hardware.';
-$lang['system_report_app_name'] = 'System Report';
-$lang['system_report_available'] = 'Available';
-$lang['system_report_cpu_model'] = 'CPU Model';
+$lang['system_report_app_description'] = 'Nu System Laporan nyadiakeun informasi dina sistem operasi jeung hardware kaayaan.';
+$lang['system_report_app_name'] = 'System Laporan';
+$lang['system_report_available'] = 'kasadia';
+$lang['system_report_cpu_model'] = 'Model CPU';
 $lang['system_report_filesystem'] = 'Filesystem';
 $lang['system_report_filesystem_summary'] = 'Filesystem Summary';
-$lang['system_report_item'] = 'Item';
-$lang['system_report_kernel_version'] = 'Kernel Version';
-$lang['system_report_load'] = 'Load';
-$lang['system_report_memory_size'] = 'Memory Size';
-$lang['system_report_mounted'] = 'Mounted';
-$lang['system_report_size'] = 'Size';
-$lang['system_report_system_details'] = 'System Details';
+$lang['system_report_item'] = 'barang';
+$lang['system_report_kernel_version'] = 'kernel Vérsi';
+$lang['system_report_load'] = 'momotkeun';
+$lang['system_report_memory_size'] = 'Ukuran memori';
+$lang['system_report_mounted'] = 'dipasang';
+$lang['system_report_size'] = 'ukuran';
+$lang['system_report_system_details'] = 'Rincian System';
 $lang['system_report_system_time'] = 'System Time';
-$lang['system_report_uptime'] = 'Uptime';
-$lang['system_report_use'] = 'Use %';
-$lang['system_report_used'] = 'Used';
-$lang['system_report_value'] = 'Value';
+$lang['system_report_uptime'] = 'uptime';
+$lang['system_report_use'] = 'Paké%';
+$lang['system_report_used'] = 'dipaké';
+$lang['system_report_value'] = 'ajen';
