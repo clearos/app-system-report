7327ec20be788f310b0b79ea092cc16fca6d876f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 02:26:58 +0000

    Add new files and directories and commit changes to local repository

--- language/vi/system_report_lang.php
+++ language/vi/system_report_lang.php
@@ -1,20 +1,20 @@
 <?php
 
-$lang['system_report_app_description'] = 'The System Report provides information on the operating system and underlying hardware.';
-$lang['system_report_app_name'] = 'System Report';
-$lang['system_report_available'] = 'Available';
-$lang['system_report_cpu_model'] = 'CPU Model';
-$lang['system_report_filesystem'] = 'Filesystem';
-$lang['system_report_filesystem_summary'] = 'Filesystem Summary';
-$lang['system_report_item'] = 'Item';
-$lang['system_report_kernel_version'] = 'Kernel Version';
-$lang['system_report_load'] = 'Load';
-$lang['system_report_memory_size'] = 'Memory Size';
+$lang['system_report_app_description'] = 'Báo cáo Hệ thống cung cấp thông tin về hệ điều hành và phần cứng cơ bản.';
+$lang['system_report_app_name'] = 'hệ thống báo cáo';
+$lang['system_report_available'] = 'có sẵn';
+$lang['system_report_cpu_model'] = 'CPU mẫu';
+$lang['system_report_filesystem'] = 'Hệ thống tập tin';
+$lang['system_report_filesystem_summary'] = 'Tóm tắt hệ thống tập tin';
+$lang['system_report_item'] = 'Mục';
+$lang['system_report_kernel_version'] = 'Phiên bản kernel';
+$lang['system_report_load'] = 'tải';
+$lang['system_report_memory_size'] = 'Kích thước bộ nhớ';
 $lang['system_report_mounted'] = 'Mounted';
-$lang['system_report_size'] = 'Size';
-$lang['system_report_system_details'] = 'System Details';
-$lang['system_report_system_time'] = 'System Time';
-$lang['system_report_uptime'] = 'Uptime';
-$lang['system_report_use'] = 'Use %';
-$lang['system_report_used'] = 'Used';
-$lang['system_report_value'] = 'Value';
+$lang['system_report_size'] = 'Kích thước';
+$lang['system_report_system_details'] = 'Các chi tiết hệ thống';
+$lang['system_report_system_time'] = 'hệ thống Time';
+$lang['system_report_uptime'] = 'Thời gian hoạt động';
+$lang['system_report_use'] = 'Sử dụng %';
+$lang['system_report_used'] = 'Đã sử dụng';
+$lang['system_report_value'] = 'Giá trị';
