0a154410adfc391a92c69dd68f43a6c4f8106f39
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 02:27:42 +0000

    Add new files and directories and commit changes to local repository

--- language/zu/system_report_lang.php
+++ language/zu/system_report_lang.php
@@ -1,20 +1,20 @@
 <?php
 
-$lang['system_report_app_description'] = 'The System Report provides information on the operating system and underlying hardware.';
-$lang['system_report_app_name'] = 'System Report';
-$lang['system_report_available'] = 'Available';
+$lang['system_report_app_description'] = 'Umbiko System inikeza ulwazi uhlelo lokusebenza hardware ecashile.';
+$lang['system_report_app_name'] = 'Umbiko System';
+$lang['system_report_available'] = 'Iyatholakala';
 $lang['system_report_cpu_model'] = 'CPU Model';
-$lang['system_report_filesystem'] = 'Filesystem';
-$lang['system_report_filesystem_summary'] = 'Filesystem Summary';
-$lang['system_report_item'] = 'Item';
-$lang['system_report_kernel_version'] = 'Kernel Version';
-$lang['system_report_load'] = 'Load';
-$lang['system_report_memory_size'] = 'Memory Size';
-$lang['system_report_mounted'] = 'Mounted';
-$lang['system_report_size'] = 'Size';
-$lang['system_report_system_details'] = 'System Details';
-$lang['system_report_system_time'] = 'System Time';
+$lang['system_report_filesystem'] = 'yamafayela wesitoreji';
+$lang['system_report_filesystem_summary'] = 'yamafayela wesitoreji Summary';
+$lang['system_report_item'] = 'Into';
+$lang['system_report_kernel_version'] = 'Inguqulo ye-kernel';
+$lang['system_report_load'] = 'Layisha';
+$lang['system_report_memory_size'] = 'Memory Usayizi';
+$lang['system_report_mounted'] = 'egibele';
+$lang['system_report_size'] = 'Usayizi';
+$lang['system_report_system_details'] = 'System Yokuxhumana';
+$lang['system_report_system_time'] = 'Isikhathi System';
 $lang['system_report_uptime'] = 'Uptime';
-$lang['system_report_use'] = 'Use %';
+$lang['system_report_use'] = 'Sebenzisa%';
 $lang['system_report_used'] = 'Used';
 $lang['system_report_value'] = 'Value';
