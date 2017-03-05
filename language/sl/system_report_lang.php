6d475da2370618b71dba11793f64a030ad7dc492
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 02:24:02 +0000

    Add new files and directories and commit changes to local repository

--- language/sl/system_report_lang.php
+++ language/sl/system_report_lang.php
@@ -1,20 +1,20 @@
 <?php
 
-$lang['system_report_app_description'] = 'The System Report provides information on the operating system and underlying hardware.';
-$lang['system_report_app_name'] = 'System Report';
-$lang['system_report_available'] = 'Available';
+$lang['system_report_app_description'] = 'Poročilo sistem zagotavlja informacije o operacijskem sistemu in osnovne strojne opreme.';
+$lang['system_report_app_name'] = 'Poročilo sistem';
+$lang['system_report_available'] = 'Na voljo';
 $lang['system_report_cpu_model'] = 'CPU Model';
-$lang['system_report_filesystem'] = 'Filesystem';
-$lang['system_report_filesystem_summary'] = 'Filesystem Summary';
-$lang['system_report_item'] = 'Item';
-$lang['system_report_kernel_version'] = 'Kernel Version';
+$lang['system_report_filesystem'] = 'Datotečni sistem';
+$lang['system_report_filesystem_summary'] = 'datotečni sistem Povzetek';
+$lang['system_report_item'] = 'Postavka';
+$lang['system_report_kernel_version'] = 'Kernel različica';
 $lang['system_report_load'] = 'Load';
 $lang['system_report_memory_size'] = 'Memory Size';
-$lang['system_report_mounted'] = 'Mounted';
-$lang['system_report_size'] = 'Size';
-$lang['system_report_system_details'] = 'System Details';
-$lang['system_report_system_time'] = 'System Time';
-$lang['system_report_uptime'] = 'Uptime';
-$lang['system_report_use'] = 'Use %';
-$lang['system_report_used'] = 'Used';
-$lang['system_report_value'] = 'Value';
+$lang['system_report_mounted'] = 'montirani';
+$lang['system_report_size'] = 'Velikost';
+$lang['system_report_system_details'] = 'sistem Podrobnosti';
+$lang['system_report_system_time'] = 'Čas sistem';
+$lang['system_report_uptime'] = 'uptime';
+$lang['system_report_use'] = 'Uporaba %';
+$lang['system_report_used'] = 'uporablja';
+$lang['system_report_value'] = 'vrednost';
