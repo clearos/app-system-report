7537e0597f0b3141564fc3eb8418d89a733a8b1d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 02:15:03 +0000

    Add new files and directories and commit changes to local repository

--- language/hr/system_report_lang.php
+++ language/hr/system_report_lang.php
@@ -1,20 +1,20 @@
 <?php
 
-$lang['system_report_app_description'] = 'The System Report provides information on the operating system and underlying hardware.';
-$lang['system_report_app_name'] = 'System Report';
-$lang['system_report_available'] = 'Available';
-$lang['system_report_cpu_model'] = 'CPU Model';
-$lang['system_report_filesystem'] = 'Filesystem';
-$lang['system_report_filesystem_summary'] = 'Filesystem Summary';
-$lang['system_report_item'] = 'Item';
-$lang['system_report_kernel_version'] = 'Kernel Version';
-$lang['system_report_load'] = 'Load';
+$lang['system_report_app_description'] = 'Izvješće sustav pruža informacije o operativnom sustavu i temeljne hardver.';
+$lang['system_report_app_name'] = 'Prijavi sustav';
+$lang['system_report_available'] = 'Dostupno';
+$lang['system_report_cpu_model'] = 'CPU model';
+$lang['system_report_filesystem'] = 'Sustav datoteka';
+$lang['system_report_filesystem_summary'] = 'datotečnog sustava Sažetak';
+$lang['system_report_item'] = 'Artikal';
+$lang['system_report_kernel_version'] = 'kernel Version';
+$lang['system_report_load'] = 'Opterećenje';
 $lang['system_report_memory_size'] = 'Memory Size';
-$lang['system_report_mounted'] = 'Mounted';
-$lang['system_report_size'] = 'Size';
-$lang['system_report_system_details'] = 'System Details';
-$lang['system_report_system_time'] = 'System Time';
+$lang['system_report_mounted'] = 'Nošeni';
+$lang['system_report_size'] = 'Veličina';
+$lang['system_report_system_details'] = 'Detalji sustav';
+$lang['system_report_system_time'] = 'Time sustav';
 $lang['system_report_uptime'] = 'Uptime';
-$lang['system_report_use'] = 'Use %';
-$lang['system_report_used'] = 'Used';
-$lang['system_report_value'] = 'Value';
+$lang['system_report_use'] = 'Koristiti %';
+$lang['system_report_used'] = 'Koristi';
+$lang['system_report_value'] = 'Vrijednost';
