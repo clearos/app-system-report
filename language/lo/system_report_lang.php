9688447421a9aca0e5e99e9ea618973a2562b8f8
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 02:19:53 +0000

    Add new files and directories and commit changes to local repository

--- language/lo/system_report_lang.php
+++ language/lo/system_report_lang.php
@@ -1,20 +1,20 @@
 <?php
 
-$lang['system_report_app_description'] = 'The System Report provides information on the operating system and underlying hardware.';
-$lang['system_report_app_name'] = 'System Report';
-$lang['system_report_available'] = 'Available';
+$lang['system_report_app_description'] = 'ບົດລາຍງານລະບົບສະຫນອງຂໍ້ມູນກ່ຽວກັບລະບົບປະຕິບັດການແລະຮາດແວທີ່ຕິດພັນ.';
+$lang['system_report_app_name'] = 'ບົດລາຍງານລະບົບ';
+$lang['system_report_available'] = 'ມີ';
 $lang['system_report_cpu_model'] = 'CPU Model';
-$lang['system_report_filesystem'] = 'Filesystem';
-$lang['system_report_filesystem_summary'] = 'Filesystem Summary';
-$lang['system_report_item'] = 'Item';
-$lang['system_report_kernel_version'] = 'Kernel Version';
+$lang['system_report_filesystem'] = 'ລະບົບແຟ້ມ';
+$lang['system_report_filesystem_summary'] = 'ລະບົບແຟ້ມ Summary';
+$lang['system_report_item'] = 'ລາຍການ';
+$lang['system_report_kernel_version'] = 'ແກ່ນ Version';
 $lang['system_report_load'] = 'Load';
-$lang['system_report_memory_size'] = 'Memory Size';
-$lang['system_report_mounted'] = 'Mounted';
-$lang['system_report_size'] = 'Size';
-$lang['system_report_system_details'] = 'System Details';
-$lang['system_report_system_time'] = 'System Time';
+$lang['system_report_memory_size'] = 'ຂະຫນາດຫນ່ວຍຄວາມຈໍາ';
+$lang['system_report_mounted'] = 'ຕິດຕັ້ງ';
+$lang['system_report_size'] = 'ຂະຫນາດ';
+$lang['system_report_system_details'] = 'ລາຍລະອຽດລະບົບ';
+$lang['system_report_system_time'] = 'ທີ່ໃຊ້ເວລາລະບົບ';
 $lang['system_report_uptime'] = 'Uptime';
-$lang['system_report_use'] = 'Use %';
-$lang['system_report_used'] = 'Used';
-$lang['system_report_value'] = 'Value';
+$lang['system_report_use'] = 'ການນໍາໃຊ້%';
+$lang['system_report_used'] = 'ການນໍາໃຊ້';
+$lang['system_report_value'] = 'ມູນຄ່າ';
