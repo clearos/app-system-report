518dfc2da88133a928b74e8e704b866feaee0fa3
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 02:15:56 +0000

    Add new files and directories and commit changes to local repository

--- language/fi/system_report_lang.php
+++ language/fi/system_report_lang.php
@@ -1,20 +1,20 @@
 <?php
 
-$lang['system_report_app_description'] = 'The System Report provides information on the operating system and underlying hardware.';
-$lang['system_report_app_name'] = 'System Report';
-$lang['system_report_available'] = 'Available';
+$lang['system_report_app_description'] = 'Järjestelmän Raportti sisältää tietoa käyttöjärjestelmästä ja taustalla laitteisto.';
+$lang['system_report_app_name'] = 'Järjestelmäraportti';
+$lang['system_report_available'] = 'Käytettävissä';
 $lang['system_report_cpu_model'] = 'CPU Model';
-$lang['system_report_filesystem'] = 'Filesystem';
-$lang['system_report_filesystem_summary'] = 'Filesystem Summary';
-$lang['system_report_item'] = 'Item';
-$lang['system_report_kernel_version'] = 'Kernel Version';
-$lang['system_report_load'] = 'Load';
-$lang['system_report_memory_size'] = 'Memory Size';
-$lang['system_report_mounted'] = 'Mounted';
-$lang['system_report_size'] = 'Size';
-$lang['system_report_system_details'] = 'System Details';
-$lang['system_report_system_time'] = 'System Time';
-$lang['system_report_uptime'] = 'Uptime';
-$lang['system_report_use'] = 'Use %';
-$lang['system_report_used'] = 'Used';
-$lang['system_report_value'] = 'Value';
+$lang['system_report_filesystem'] = 'Tiedostojärjestelmä';
+$lang['system_report_filesystem_summary'] = 'tiedostojärjestelmän Yhteenveto';
+$lang['system_report_item'] = 'erä';
+$lang['system_report_kernel_version'] = 'kernel versio';
+$lang['system_report_load'] = 'Ladata';
+$lang['system_report_memory_size'] = 'muistin koko';
+$lang['system_report_mounted'] = 'asennettu';
+$lang['system_report_size'] = 'Koko';
+$lang['system_report_system_details'] = 'järjestelmän tiedot';
+$lang['system_report_system_time'] = 'Järjestelmän aika';
+$lang['system_report_uptime'] = 'Päällä';
+$lang['system_report_use'] = 'Käytä%';
+$lang['system_report_used'] = 'Käytetyt';
+$lang['system_report_value'] = 'Arvo';
