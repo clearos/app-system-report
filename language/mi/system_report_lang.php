7846060286c679dbc7f99161380b41ee04e660b6
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 02:20:35 +0000

    Add new files and directories and commit changes to local repository

--- language/mi/system_report_lang.php
+++ language/mi/system_report_lang.php
@@ -1,20 +1,20 @@
 <?php
 
-$lang['system_report_app_description'] = 'The System Report provides information on the operating system and underlying hardware.';
-$lang['system_report_app_name'] = 'System Report';
-$lang['system_report_available'] = 'Available';
-$lang['system_report_cpu_model'] = 'CPU Model';
-$lang['system_report_filesystem'] = 'Filesystem';
-$lang['system_report_filesystem_summary'] = 'Filesystem Summary';
-$lang['system_report_item'] = 'Item';
-$lang['system_report_kernel_version'] = 'Kernel Version';
-$lang['system_report_load'] = 'Load';
-$lang['system_report_memory_size'] = 'Memory Size';
+$lang['system_report_app_description'] = 'E whakarato ana te Pūrongo Pūnaha mōhiohio i runga i te pūnaha mahi me te taputapu e whāriki ana.';
+$lang['system_report_app_name'] = 'Pūrongo pūnaha';
+$lang['system_report_available'] = 'wātea';
+$lang['system_report_cpu_model'] = 'Tauira PTM';
+$lang['system_report_filesystem'] = 'Pūnahakōnae';
+$lang['system_report_filesystem_summary'] = 'Whakarāpopoto Pūnahakōnae';
+$lang['system_report_item'] = 'Te nama take';
+$lang['system_report_kernel_version'] = 'kākano Putanga';
+$lang['system_report_load'] = 'Utaina';
+$lang['system_report_memory_size'] = 'Rahi Pūmahara';
 $lang['system_report_mounted'] = 'Mounted';
-$lang['system_report_size'] = 'Size';
-$lang['system_report_system_details'] = 'System Details';
-$lang['system_report_system_time'] = 'System Time';
-$lang['system_report_uptime'] = 'Uptime';
-$lang['system_report_use'] = 'Use %';
-$lang['system_report_used'] = 'Used';
-$lang['system_report_value'] = 'Value';
+$lang['system_report_size'] = 'rahi';
+$lang['system_report_system_details'] = 'Taipitopito pūnaha';
+$lang['system_report_system_time'] = 'Wā pūnaha';
+$lang['system_report_uptime'] = 'wāmahi';
+$lang['system_report_use'] = 'Whakamahia%';
+$lang['system_report_used'] = 'whakamahia';
+$lang['system_report_value'] = 'uara';
