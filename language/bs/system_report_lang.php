7161d9be55ffaf2f4d34a0d5a18e9f2d9913e888
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 02:13:59 +0000

    Add new files and directories and commit changes to local repository

--- language/bs/system_report_lang.php
+++ language/bs/system_report_lang.php
@@ -1,20 +1,20 @@
 <?php
 
-$lang['system_report_app_description'] = 'The System Report provides information on the operating system and underlying hardware.';
+$lang['system_report_app_description'] = 'Prijavi System pruža informacije o operativnom sistemu i osnovni hardver.';
 $lang['system_report_app_name'] = 'System Report';
-$lang['system_report_available'] = 'Available';
-$lang['system_report_cpu_model'] = 'CPU Model';
+$lang['system_report_available'] = 'dostupan';
+$lang['system_report_cpu_model'] = 'Model procesora';
 $lang['system_report_filesystem'] = 'Filesystem';
-$lang['system_report_filesystem_summary'] = 'Filesystem Summary';
-$lang['system_report_item'] = 'Item';
-$lang['system_report_kernel_version'] = 'Kernel Version';
-$lang['system_report_load'] = 'Load';
-$lang['system_report_memory_size'] = 'Memory Size';
-$lang['system_report_mounted'] = 'Mounted';
-$lang['system_report_size'] = 'Size';
-$lang['system_report_system_details'] = 'System Details';
+$lang['system_report_filesystem_summary'] = 'filesystem Pregled';
+$lang['system_report_item'] = 'stavka';
+$lang['system_report_kernel_version'] = 'kernel Version';
+$lang['system_report_load'] = 'opterećenje';
+$lang['system_report_memory_size'] = 'Veličina memorije';
+$lang['system_report_mounted'] = 'montiran';
+$lang['system_report_size'] = 'Veličina';
+$lang['system_report_system_details'] = 'System Detalji';
 $lang['system_report_system_time'] = 'System Time';
 $lang['system_report_uptime'] = 'Uptime';
-$lang['system_report_use'] = 'Use %';
-$lang['system_report_used'] = 'Used';
-$lang['system_report_value'] = 'Value';
+$lang['system_report_use'] = 'Koristite%';
+$lang['system_report_used'] = 'Rabljeni';
+$lang['system_report_value'] = 'vrijednost';
