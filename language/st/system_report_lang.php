40df4ef89bcc9ef29493cb04b9c08b7b15f1f226
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 02:24:24 +0000

    Add new files and directories and commit changes to local repository

--- language/st/system_report_lang.php
+++ language/st/system_report_lang.php
@@ -1,20 +1,20 @@
 <?php
 
-$lang['system_report_app_description'] = 'The System Report provides information on the operating system and underlying hardware.';
-$lang['system_report_app_name'] = 'System Report';
-$lang['system_report_available'] = 'Available';
+$lang['system_report_app_description'] = 'The Report System fana ka tlhahisoleseding e mabapi le tsamaiso ea ho sebelisa mehaho le hardware se ipatileng.';
+$lang['system_report_app_name'] = 'Report System';
+$lang['system_report_available'] = 'fumaneha';
 $lang['system_report_cpu_model'] = 'CPU Model';
 $lang['system_report_filesystem'] = 'Filesystem';
 $lang['system_report_filesystem_summary'] = 'Filesystem Summary';
 $lang['system_report_item'] = 'Item';
 $lang['system_report_kernel_version'] = 'Kernel Version';
-$lang['system_report_load'] = 'Load';
-$lang['system_report_memory_size'] = 'Memory Size';
-$lang['system_report_mounted'] = 'Mounted';
+$lang['system_report_load'] = 'mojaro';
+$lang['system_report_memory_size'] = 'memori size';
+$lang['system_report_mounted'] = 'mounted';
 $lang['system_report_size'] = 'Size';
-$lang['system_report_system_details'] = 'System Details';
+$lang['system_report_system_details'] = 'Details System';
 $lang['system_report_system_time'] = 'System Time';
 $lang['system_report_uptime'] = 'Uptime';
-$lang['system_report_use'] = 'Use %';
-$lang['system_report_used'] = 'Used';
-$lang['system_report_value'] = 'Value';
+$lang['system_report_use'] = 'sebelisa%';
+$lang['system_report_used'] = 'sebelisoa';
+$lang['system_report_value'] = 'latela boleng ba';
