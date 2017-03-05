552aebdcce270e6ed43e94693109a9a25bf1c4cd
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 02:25:41 +0000

    Add new files and directories and commit changes to local repository

--- language/tg/system_report_lang.php
+++ language/tg/system_report_lang.php
@@ -1,20 +1,20 @@
 <?php
 
-$lang['system_report_app_description'] = 'The System Report provides information on the operating system and underlying hardware.';
-$lang['system_report_app_name'] = 'System Report';
-$lang['system_report_available'] = 'Available';
-$lang['system_report_cpu_model'] = 'CPU Model';
-$lang['system_report_filesystem'] = 'Filesystem';
-$lang['system_report_filesystem_summary'] = 'Filesystem Summary';
-$lang['system_report_item'] = 'Item';
-$lang['system_report_kernel_version'] = 'Kernel Version';
-$lang['system_report_load'] = 'Load';
-$lang['system_report_memory_size'] = 'Memory Size';
-$lang['system_report_mounted'] = 'Mounted';
-$lang['system_report_size'] = 'Size';
-$lang['system_report_system_details'] = 'System Details';
-$lang['system_report_system_time'] = 'System Time';
+$lang['system_report_app_description'] = 'Системаи Њисобот маълумот дар бораи системаи амалиётӣ ва сахтафзор аслї таъмин менамояд.';
+$lang['system_report_app_name'] = 'Системаи Њисобот оид ба';
+$lang['system_report_available'] = 'дастрас';
+$lang['system_report_cpu_model'] = 'модели CPU';
+$lang['system_report_filesystem'] = 'файлии';
+$lang['system_report_filesystem_summary'] = 'системаи файлии хулоса';
+$lang['system_report_item'] = 'Банди';
+$lang['system_report_kernel_version'] = 'ядро Version';
+$lang['system_report_load'] = 'сарборӣ';
+$lang['system_report_memory_size'] = 'Андоза хотира';
+$lang['system_report_mounted'] = 'насбшударо';
+$lang['system_report_size'] = 'андоза';
+$lang['system_report_system_details'] = 'Системаи Cохта шуд';
+$lang['system_report_system_time'] = 'Системаи Time';
 $lang['system_report_uptime'] = 'Uptime';
-$lang['system_report_use'] = 'Use %';
-$lang['system_report_used'] = 'Used';
-$lang['system_report_value'] = 'Value';
+$lang['system_report_use'] = 'истифода баред%';
+$lang['system_report_used'] = 'Истифода бурда мешавад,';
+$lang['system_report_value'] = 'арзиши';
