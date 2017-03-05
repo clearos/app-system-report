f99d484dd0929e26b364f8dd334b548ff77ce00f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 02:22:12 +0000

    Add new files and directories and commit changes to local repository

--- language/no/system_report_lang.php
+++ language/no/system_report_lang.php
@@ -1,20 +1,20 @@
 <?php
 
-$lang['system_report_app_description'] = 'The System Report provides information on the operating system and underlying hardware.';
-$lang['system_report_app_name'] = 'System Report';
-$lang['system_report_available'] = 'Available';
+$lang['system_report_app_description'] = 'Systemrapporten gir informasjon om operativsystemet og underliggende maskinvaren.';
+$lang['system_report_app_name'] = 'system~~POS=TRUNC';
+$lang['system_report_available'] = 'Tilgjengelig';
 $lang['system_report_cpu_model'] = 'CPU Model';
-$lang['system_report_filesystem'] = 'Filesystem';
-$lang['system_report_filesystem_summary'] = 'Filesystem Summary';
-$lang['system_report_item'] = 'Item';
-$lang['system_report_kernel_version'] = 'Kernel Version';
-$lang['system_report_load'] = 'Load';
-$lang['system_report_memory_size'] = 'Memory Size';
-$lang['system_report_mounted'] = 'Mounted';
-$lang['system_report_size'] = 'Size';
-$lang['system_report_system_details'] = 'System Details';
-$lang['system_report_system_time'] = 'System Time';
-$lang['system_report_uptime'] = 'Uptime';
-$lang['system_report_use'] = 'Use %';
-$lang['system_report_used'] = 'Used';
-$lang['system_report_value'] = 'Value';
+$lang['system_report_filesystem'] = 'Filsystem';
+$lang['system_report_filesystem_summary'] = 'filsystem oppsummering';
+$lang['system_report_item'] = 'Punkt';
+$lang['system_report_kernel_version'] = 'Kernel versjon';
+$lang['system_report_load'] = 'Laste';
+$lang['system_report_memory_size'] = 'Minnestørrelse';
+$lang['system_report_mounted'] = 'montert';
+$lang['system_report_size'] = 'Størrelse';
+$lang['system_report_system_details'] = 'system~~POS=TRUNC';
+$lang['system_report_system_time'] = 'System tid';
+$lang['system_report_uptime'] = 'oppetid';
+$lang['system_report_use'] = 'Bruk %';
+$lang['system_report_used'] = 'brukes';
+$lang['system_report_value'] = 'Verdi';
