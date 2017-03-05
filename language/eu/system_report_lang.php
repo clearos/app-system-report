29f61db38433a3e0ecd2001fd1bf4074594d0a90
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 02:13:27 +0000

    Add new files and directories and commit changes to local repository

--- language/eu/system_report_lang.php
+++ language/eu/system_report_lang.php
@@ -1,20 +1,20 @@
 <?php
 
-$lang['system_report_app_description'] = 'The System Report provides information on the operating system and underlying hardware.';
-$lang['system_report_app_name'] = 'System Report';
-$lang['system_report_available'] = 'Available';
+$lang['system_report_app_description'] = 'Sistema Txostena sistema eragilearen eta azpiko hardware buruzko informazioa soilik eskaintzen du.';
+$lang['system_report_app_name'] = 'Sistema txostena';
+$lang['system_report_available'] = 'Eskuragarri';
 $lang['system_report_cpu_model'] = 'CPU Model';
 $lang['system_report_filesystem'] = 'Filesystem';
-$lang['system_report_filesystem_summary'] = 'Filesystem Summary';
+$lang['system_report_filesystem_summary'] = 'Filesystem Laburpena';
 $lang['system_report_item'] = 'Item';
 $lang['system_report_kernel_version'] = 'Kernel Version';
 $lang['system_report_load'] = 'Load';
-$lang['system_report_memory_size'] = 'Memory Size';
-$lang['system_report_mounted'] = 'Mounted';
-$lang['system_report_size'] = 'Size';
-$lang['system_report_system_details'] = 'System Details';
-$lang['system_report_system_time'] = 'System Time';
-$lang['system_report_uptime'] = 'Uptime';
-$lang['system_report_use'] = 'Use %';
-$lang['system_report_used'] = 'Used';
-$lang['system_report_value'] = 'Value';
+$lang['system_report_memory_size'] = 'Memoria Tamaina';
+$lang['system_report_mounted'] = 'Zaldizko';
+$lang['system_report_size'] = 'Tamaina';
+$lang['system_report_system_details'] = 'Sistema xehetasunak';
+$lang['system_report_system_time'] = 'Sistemaren ordua';
+$lang['system_report_uptime'] = 'Hauekin';
+$lang['system_report_use'] = 'erabili%';
+$lang['system_report_used'] = 'Erabilitako';
+$lang['system_report_value'] = 'Balio';
